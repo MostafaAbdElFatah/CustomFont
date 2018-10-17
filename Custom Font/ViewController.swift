59cddf44d52bc82798691c827d7a729ff5831f45
Mostafa AbdEl Fatah <mostafaabdelfatah@Mostafas-Mac.local>
2018-10-17 07:50:52 -0700

    first commit

--- Custom Font/ViewController.swift
+++ Custom Font/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label0: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.printFonts()
+        
+        label0.font = UIFont(name: "DroidArabicNaskh", size: 17)
+        label0.sizeToFit()
+        label1.font = UIFont(name: "DroidArabicNaskh-Bold", size: 17)
+        label1.sizeToFit()
+        label2.font = UIFont(name: "AD-Rsail", size: 17)
+        label2.sizeToFit()
+        label3.font = UIFont(name: "DroidSansArabic", size: 17)
+        label3.sizeToFit()
+        label4.font = UIFont(name: "ANegaar", size: 17)
+        label4.sizeToFit()
+        label5.font = UIFont(name: "ANegaar-Bold", size: 17)
+        label5.sizeToFit()
+        label6.font = UIFont(name: "SaneJalehFont-Bold", size: 17)
+        label6.sizeToFit()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func printFonts(){
+        for family in UIFont.familyNames {
+            print("## \(family) ##")
+            for name in UIFont.fontNames(forFamilyName: family){
+                print("\(name)")
+            }
+            print("\n")
+        }
     }
-
 
 }
 
